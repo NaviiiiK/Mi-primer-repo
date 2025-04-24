1c056b76cb5d87239884952790aef9097bd0c3fd
DAW1 <daw202614@estudiantes.salesianasnsp.es>
2025-04-24 12:16:27 +0200

    Proyecto añadido

---
+++ ejercicio1.java
@@ -0,0 +1,13 @@
+package ejercicios;
+
+public class ejercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*System.out.println("Me gusta la progrmación");
+		System.out.println("Cada día");
+		System.out.println("MASS");*/
+		System.out.println("Me gusta la progrmación\nCada dia\nMASS");
+	}
+
+}
